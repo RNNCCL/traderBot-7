3d713581aeaee0b62d20434ba89264f1c92cd615
Arthurgit config --global user.email a-rakhmatullin2013@yandex.ru <a-rakhmatullin2013@yandex.ru>
2017-10-18 15:33:52 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,174 @@
+import telebot
+import logging
+import const
+import markups
+import sqlite3
+import datetime
+import parser
+import time
+import requests
+import hashlib
+from aiohttp import web
+
+
+
+WEBHOOK_HOST = '85.143.175.233'
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP address
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'
+
+WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/{}/".format(const.token)
+
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
+loggerPy = logging.getLogger('bot.py')
+loggerPy.setLevel(logging.DEBUG)
+ch = logging.FileHandler('journal.log')
+formatter = logging.Formatter('%(levelname)s : DATE ---> %(asctime)s - %(message)s')
+ch.setFormatter(formatter)
+loggerPy.addHandler(ch)
+
+bot = telebot.TeleBot(const.token)
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    with sqlite3.connect(const.dbName) as db:
+        cursor = db.cursor()
+        cursor.execute('INSERT INTO users (uid) VALUES (?)', (str(message.chat.id),))
+        db.commit()
+    bot.send_message(message.chat.id, const.startMsg, reply_markup=markups.mainMenu())
+
+
+@bot.message_handler(regexp="Маркетинг")
+def marketing(message):
+    bot.send_message(message.chat.id, const.marketingMsg)
+
+
+@bot.message_handler(regexp="Маркетинговые материалы")
+def materials(message):
+    bot.send_message(message.chat.id, "Выберите маркетинговый материал", reply_markup=markups.materials())
+
+
+@bot.message_handler(regexp="Начать работу")
+def startWork(message):
+    bot.send_message(message.chat.id, const.startWorkMsg, reply_markup=markups.startWork())
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "conditions")
+def showConditions(call):
+    bot.send_message(call.message.chat.id, const.conditionsMsg)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "news")
+def channelLink(call):
+    bot.send_message(call.message.chat.id, const.channelLink)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "socialNetworks")
+def showMedia(call):
+    bot.edit_message_text("текст", call.message.chat.id, call.message.message_id)
+    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=markups.socialNetworks())
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "profit")
+def showProfit(call):
+    bot.send_message(call.message.chat.id, const.profitMsg, reply_markup=markups.payBtnMarkup())
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "processPayment")
+def chooseDuration(call):
+    bot.send_message(call.message.chat.id, "Выберите подписку", reply_markup=markups.chooseDuration())
+
+
+@bot.callback_query_handler(func=lambda call: call.data[:4] == "days")
+def processPayment(call):
+    days = call.data[4:]
+    if days == "15":
+        pay = const.days15
+    elif days == "30":
+        pay = const.days30
+    elif days == "60":
+        pay = const.days60
+    else:
+        pay = const.days90
+    address = createBTCAddress()
+    bot.send_message(call.message.chat.id, const.paymentMsg.format(pay, address), parse_mode="html")
+
+
+def createBTCAddress():
+    sign = hashlib.md5("".join((const.wallet_id, const.walletApiKey)).encode()).hexdigest()
+    data = {
+        "wallet_id": const.wallet_id,
+        "sign": sign,
+        "action": "create_btc_address"
+    }
+    url = "https://wallet.free-kassa.ru/api_v1.php"
+    response = requests.post(url, data).json()
+    return response.get("data").get("address")
+
+
+def daily_check():
+    with sqlite3.connect(const.dbName) as db:
+        cursor = db.cursor()
+        sql = 'SELECT uid, end_date FROM payments'
+        res = cursor.execute(sql).fetchall()
+        today = str(datetime.datetime.now()).split(' ')[0]
+        after_tomorrow = parser.parse(today) + datetime.timedelta(days=2)
+        print(after_tomorrow)
+        for user in res:
+            if after_tomorrow == parser.parse(str(user[1])):
+                text = 'У вас истекает подписка.'
+                bot.send_message(user[0], text)
+                send_payment_message(user[0])
+                time.sleep(0.1)
+            if parser.parse(str(user[1])) <= parser.parse(today):
+                text = 'Время действия вашей подписки окончено.'
+                bot.send_message(user[0], text)
+                send_payment_message(user[0])
+                # Delete user from DataBase
+                sql = 'DELETE FROM payments WHERE uid=?'
+                cursor.execute(sql, (user[0],))
+                time.sleep(0.1)
+        db.commit()
+
+
+def send_payment_message(cid):
+    pass
+
+
+# if __name__ == '__main__':
+#     with sqlite3.connect(const.dbName) as db:
+#         cursor = db.cursor()
+#         sql = '''CREATE TABLE IF NOT EXISTS payments (
+#             id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+#             uid INTEGER NOT NULL,
+#             end_date TEXT NOT NULL)'''
+#         cursor.execute(sql)
+#         db.commit()
+#     bot.remove_webhook()
+#     bot.polling()
+
+
+
+app = web.Application()
+
+
+
+# Process webhook calls
+async def handle(request):
+    if request.match_info.get('token') == bot.token:
+        request_body_dict = await request.json()
+        update = telebot.types.Update.de_json(request_body_dict)
+        bot.process_new_updates([update])
+        return web.Response()
+    else:
+        return web.Response(status=403)
+
+app.router.add_post('/{token}/', handle)
+
---
+++ const.py
@@ -0,0 +1,22 @@
+token = "317025779:AAFLih4bH_BjKIm8_giC-wD1geEd8d_tLCI"
+startMsg = "Сюда вставить стартовый текст (приветственное сообщение)"
+marketingMsg = "Сообщение при выборе маркетинга"
+startWorkMsg = "Сообщегние при нажати начать работу"
+conditionsMsg = "Условия"
+channelLink = "link on a channel"
+profitMsg = "profit"
+packetsMsg = "set of packets"
+paymentMsg = 'Отправьте точно <b>{0}</b> BTC на адрес <b>{1}</b> ' \
+             'Ваша подписка будет автоматически активирована после одного подтверждения в сети BitCoin. ' \
+             'Скорость получения подписки зависит от установленной Вами комиссии транзакции. '
+
+days15 = 0.018
+days30 = 0.030
+days60 = 0.055
+days90 = 0.07
+
+dbName = "clientbase.db"
+
+wallet_id = "F102745422"
+walletApiKey = "8B8793CAC68F8CE9678D9D907A0E07BB"
+
