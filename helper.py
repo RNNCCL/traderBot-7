799097d12ba8e2e92c89d7fda9a7b18640735c73
Arthurgit config --global user.email a-rakhmatullin2013@yandex.ru <a-rakhmatullin2013@yandex.ru>
2017-11-09 11:44:32 +0300

    update

--- helper.py
+++ helper.py
@@ -3,10 +3,12 @@ import datetime
 import time
 
 
-def init_interval():
 
-    while True:
-        clock = str(datetime.datetime.now()).split(' ')[1][:5]
-        #if clock == '00:00':
-        daily_check()
-        time.sleep(60)
+
+while True:
+    clock = str(datetime.datetime.now()).split(' ')[1][:5]
+    #if clock == '00:00':
+    daily_check()
+    print("chaecked")
+    break
+    time.sleep(60)
